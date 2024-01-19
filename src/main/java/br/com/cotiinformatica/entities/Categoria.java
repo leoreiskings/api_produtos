8e2d506a288bee5ac1a55b513542ed60b684402b
leoreiskings <leo19reis@gmail.com>
2024-01-19 17:07:12 -0300

    Criando as entidades (Categoria e Produto) e a Estrutura (packages) do Projeto

---
+++ src/main/java/br/com/cotiinformatica/entities/Categoria.java
@@ -0,0 +1,17 @@
+package br.com.cotiinformatica.entities;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Categoria {
+
+	
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private List<Produto> produtos;
+	
+	
+}
